f5971951084df3d642285874b08ed5445a065418
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-12-04 22:16:44 -0800

    refactored part 1 code + added part 2 code

--- DecSecond.js
+++ DecSecond.js
@@ -1,5 +1,7 @@
 var fs = require("fs");
-var sum = 0 ;
+
+var totallWrapperLength = 0 ;
+var totalRibbonLength = 0;
 
 fs.readFile("DecSecondInput.txt", function(error, data){
 	var combinations = data.toString().split("\n");
@@ -7,11 +9,15 @@ fs.readFile("DecSecondInput.txt", function(error, data){
 	for(var i = 0; i < combinations.length; i++){
 
 		var currentCombo = combinations[i].split("x");
-		var smallestSide = 
-		sum +=wrapperLength(currentCombo[0], currentCombo[1], currentCombo[2]);
+		
+		totallWrapperLength +=wrapperLength(currentCombo[0], currentCombo[1], currentCombo[2]);
+
+		totalRibbonLength += ribbonLength(currentCombo[0], currentCombo[1], currentCombo[2]);
 	}
 
-	console.log(sum);
+	console.log(totallWrapperLength);
+	console.log(totalRibbonLength);
+
 
 });
 
@@ -22,15 +28,36 @@ function area(length, width){
 
 function surfaceArea(length, width, height){
 
-	return (2 * length * width) + (2 * width * height) + (2 * height * length);
+	return 2 * (area(length, width) + area(length, height) + area(height, length));
 }
 
 function areaOfSmallestSide(length,width, height){
 
- 	return Math.min((length * width), (length * height), (width * height));
+ 	return Math.min(area(length, width), area(length, height), area(width, height));
  }
 
 function wrapperLength(length, width, height){
 
 	return surfaceArea(length, width, height) + areaOfSmallestSide(length, width, height);
 }
+
+function perimeter(length, width){
+	return 2 * ( length + width);
+}
+
+function smallestPerimeters(length, width, height){
+	return Math.min(perimeter(length, width), perimeter(length, height), perimeter(width, height));
+}
+
+function bowRibbonLength(length, width, height){
+
+	return length*width*height;
+}
+
+function ribbonLength(length, width, height){
+	return smallestPerimeters(length, width, height) + bowRibbonLength(length,width,height);
+}
+
+console.log(ribbonLength(2,3,4));
+console.log(ribbonLength(1,1,10));
+
