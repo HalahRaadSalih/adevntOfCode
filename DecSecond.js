3669d39a607ef5b5a154f37a1d2f8e83ab771331
HalahRaadSalih <hala.raad.salih@gmail.com>
2015-12-04 21:50:36 -0800

    added solution for second day part 1

--- DecSecond.js
+++ DecSecond.js
@@ -1,15 +1,36 @@
 var fs = require("fs");
+var sum = 0 ;
 
 fs.readFile("DecSecondInput.txt", function(error, data){
-	var numberXRegex = /\d+\x\d+\x\d+/g;
 	var combinations = data.toString().split("\n");
 
 	for(var i = 0; i < combinations.length; i++){
 
-		console.log(combinations[i].match(numberXRegex));
+		var currentCombo = combinations[i].split("x");
+		var smallestSide = 
+		sum +=wrapperLength(currentCombo[0], currentCombo[1], currentCombo[2]);
 	}
 
-	
+	console.log(sum);
+
 });
 
+function area(length, width){
 
+	return length * width;
+}
+
+function surfaceArea(length, width, height){
+
+	return (2 * length * width) + (2 * width * height) + (2 * height * length);
+}
+
+function areaOfSmallestSide(length,width, height){
+
+ 	return Math.min((length * width), (length * height), (width * height));
+ }
+
+function wrapperLength(length, width, height){
+
+	return surfaceArea(length, width, height) + areaOfSmallestSide(length, width, height);
+}
